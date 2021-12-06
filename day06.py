95f1e8a04d28be9bc2c26111df8533d661fe7f35
Marc S. Schneider <marc.s.schneider@mailbox.org>
2021-12-06 06:55:37 +0100

    Day 6

--- day06.py
+++ day06.py
@@ -16,7 +16,7 @@ def run(fish, nrEpochs):
     for i in range(nrEpochs):
         spawncount = sum([f[1] for f in fish if f[0] == 0])
         fish = [(i[0]-1, i[1]) if i[0]-1 >= 0 else (6,i[1]) for i in fish]
-        if (spawncount > 0):
+        if spawncount > 0:
             fish.append((8, spawncount))
         # print(f"epoch {i}: length {len(fish)}")
     return sum(f[1] for f in fish)
