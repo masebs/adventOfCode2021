cc5600cb5c76ed434ea534240fb459ccba100a93
Marc S. Schneider <marc.s.schneider@mailbox.org>
2021-12-07 19:26:50 +0100

    Day 7 faster

--- day07.py
+++ day07.py
@@ -5,7 +5,7 @@ Advent of Code 2021
 @author marc 
 """
 
-with open("input-mday07", 'r') as f:
+with open("input-day07", 'r') as f:
 # with open("input-day07-test", 'r') as f:
     lines = f.readlines()
     hpos = lines[0][:-1].split(',')
@@ -22,8 +22,12 @@ opthpos = hpos[minidx]
 print(f"Task 1: Result is {minfuel}")
 
 # Very inefficient but works for this case; already computed data should be reused
+# def alignAt2(hpos, alignpos):
+#     return sum(sum(range(abs(h-alignpos)+1)) for h in hpos)
+# Use Gauss' summation formula to calculate 1+2+...+abs(h-alignpos)
+# Faster than calculating sum by a factor of at least 20
 def alignAt2(hpos, alignpos):
-    return sum(sum(range(abs(h-alignpos)+1)) for h in hpos)
+    return int(sum(0.5*abs(h-alignpos)*(abs(h-alignpos)+1) for h in hpos)) 
 
 fuel = [alignAt2(hpos, p) for p in range(max(hpos))]
 minfuel = min(fuel)
