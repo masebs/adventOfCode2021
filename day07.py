a3652a4beec5ba74f9820935001ffe7e10989a7b
Marc S. Schneider <marc.s.schneider@mailbox.org>
2021-12-07 07:47:51 +0100

    Day 7

---
+++ day07.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2021
+
+@author marc 
+"""
+
+with open("input-mday07", 'r') as f:
+# with open("input-day07-test", 'r') as f:
+    lines = f.readlines()
+    hpos = lines[0][:-1].split(',')
+    hpos = [int(i) for i in hpos]
+
+def alignAt(hpos, alignpos):
+    return sum(abs(h-alignpos) for h in hpos)
+
+fuel = [alignAt(hpos, p) for p in range(max(hpos))]
+minfuel = min(fuel)
+minidx = fuel.index(minfuel)
+opthpos = hpos[minidx]
+
+print(f"Task 1: Result is {minfuel}")
+
+# Very inefficient but works for this case; already computed data should be reused
+def alignAt2(hpos, alignpos):
+    return sum(sum(range(abs(h-alignpos)+1)) for h in hpos)
+
+fuel = [alignAt2(hpos, p) for p in range(max(hpos))]
+minfuel = min(fuel)
+minidx = fuel.index(minfuel)
+opthpos = hpos[minidx]
+
+print(f"Task 2: Result is {minfuel}")
