6178bb46b7765f97a0d45c8fc0ef2f8a20becb39
Marc S. Schneider <marc.s.schneider@mailbox.org>
2021-12-08 18:55:13 +0100

    Day 8

--- day08.py
+++ day08.py
@@ -5,14 +5,15 @@ Advent of Code 2021
 @author marc 
 """
 
-# with open("input-day08", 'r') as f:
-with open("input-day08-test", 'r') as f:
+with open("input-day08", 'r') as f:
+# with open("input-day08-test", 'r') as f:
     lines = f.readlines()
 
-lines    = [l[:-1].split('|') for l in lines]
+lines    = [l.split('|') for l in lines]
 patterns = [l[0].split() for l in lines]
 output   = [l[1].split() for l in lines]
 
+### Part 1 (could be obtained from within Part 2)
 def yieldByLength(length, valuelists):
     for l in valuelists:
         for v in l:
@@ -33,7 +34,44 @@ count8 = [v for v in yieldByLength(7, output)] # eights have length 7
 print(f"Task 1: 1, 4, 7, 8 appear {len(count1)}, {len(count4)}, {len(count7)}, {len(count8)} times, " 
       f"in total: {sum([len(count1), len(count4), len(count7), len(count8)])} times");
 
-# segments c,f are the ones included in all four easy values 1,4,7,8 
+### Part 2
+# Decodes a 7-digit representation (by segment letters) to the corresponding decimal number
+def decode(digit):
+    if len(digit) == 7:
+        return 8
+    elif len(digit) == 6:
+        if set(digit) == set(['a', 'b', 'c', 'e', 'f', 'g']):
+            return 0
+        elif set(digit) == set(['a', 'b', 'd', 'e', 'f', 'g']):
+            return 6
+        elif set(digit) == set(['a', 'b', 'c', 'd', 'f', 'g']):
+            return 9
+        else:
+            print("WARNING: Unmatched digit!")
+    elif len(digit) == 5:
+        if set(digit) == set(['a', 'b', 'c', 'e', 'f', 'g']):
+            return 0
+        elif set(digit) == set(['a', 'b', 'd', 'e', 'f', 'g']):
+            return 6
+        elif set(digit) == set(['a', 'c', 'd', 'e', 'g']):
+            return 2
+        elif set(digit) == set(['a', 'c', 'd', 'f', 'g']):
+            return 3
+        elif set(digit) == set(['a', 'b', 'd', 'f', 'g']):
+            return 5
+        else:
+            print("WARNING: Unmatched digit!")
+    elif len(digit) == 4:
+        return 4
+    elif len(digit) == 3:
+        return 7
+    elif len(digit) == 2:
+        return 1
+    else:
+        print("WARNING: Unmatched digit!")
+
+# Use the patterns to find the mapping from the actual (standard) segment to the rewired segment:
+# segments c,f are the ones from the ones (length 2)
 # segment a is the one from 7 which is not c,f -> a identified
 # segments b,d are the ones from 4 which are not c,f
 # segments e,g are the ones from 8 which are not in c,f,a,b,d
@@ -46,41 +84,60 @@ print(f"Task 1: 1, 4, 7, 8 appear {len(count1)}, {len(count4)}, {len(count7)}, {
 # segment e is the last unidentified
 
 mapping = {}
+totalsum = 0
 
-pat = patterns[0]
-len1   = [v for v in pat if len(v)==2]
-len4   = [v for v in pat if len(v)==4]
-len7   = [v for v in pat if len(v)==3]
-len8   = [v for v in pat if len(v)==7]
-len069 = [v for v in pat if len(v)==6]
-len235 = [v for v in pat if len(v)==5]
-
-s1   = set(s for el in len1 for s in el) 
-s4   = set(s for el in len4 for s in el)
-s7   = set(s for el in len7 for s in el)
-s8   = set(s for el in len8 for s in el)
-s069 = [set(l) for l in len069] 
-s235 = [set(l) for l in len235] 
-
-identifiedSet = set()
-
-mapping['cf'] = list(s1) 
-mapping['a']  = list(s7 - (s1 & s4 & s7 & s8))[0]
-identifiedSet |= set(mapping['a'])
-mapping['bd'] = list(s4 - set(mapping['cf']))
-mapping['eg'] = list(s8 - (set(mapping['a']) | set(mapping['cf']) | set(mapping['bd'])))
-mapping['c']  = list( ((s069[0] | s069[1] | s069[2]) - (s069[0] & s069[1] & s069[2]) ) & set(mapping['cf']) )
-mapping['f']  = list(set(mapping['cf']) - set(mapping['c']))
-mapping['d']  = list( ((s069[0] | s069[1] | s069[2]) - (s069[0] & s069[1] & s069[2]) ) & set(mapping['bd']) )
-mapping['b']  = list(set(mapping['bd']) - set(mapping['d']))
-mapping['g']  = list( ( (s235[0] & s235[1] & s235[2]) 
-                       & ({'a', 'b', 'c', 'd', 'e', 'f', 'g'} 
-                          - (set(mapping['a']) | set(mapping['b']) | set(mapping['c']) | set(mapping['d']) | set(mapping['f']) ) )
-                       ))
-mapping['e']  = list({'a', 'b', 'c', 'd', 'e', 'f', 'g'} 
-                     - (set(mapping['a']) | set(mapping['b']) | set(mapping['c']) | set(mapping['d']) | set(mapping['f']) | set(mapping['g']) ))
-
-def decodeOutput(output, mapping):
-    for o in output:
+for (pat, out) in zip(patterns, output):
+    
+    # Sort the representations of the pattern by their length 
+    len1   = [v for v in pat if len(v)==2]
+    len4   = [v for v in pat if len(v)==4]
+    len7   = [v for v in pat if len(v)==3]
+    len8   = [v for v in pat if len(v)==7]
+    len069 = [v for v in pat if len(v)==6]
+    len235 = [v for v in pat if len(v)==5]
+    
+    # Create sets containing the single letters for each list from above
+    s1   = set(s for el in len1 for s in el) 
+    s4   = set(s for el in len4 for s in el)
+    s7   = set(s for el in len7 for s in el)
+    s8   = set(s for el in len8 for s in el)
+    s069 = [set(l) for l in len069] 
+    s235 = [set(l) for l in len235] 
+    
+    # Apply the rules (set operations) listed above to obtain the mapping
+    mapping['cf'] = list(s1) 
+    mapping['a']  = list(s7 - set(mapping['cf']))
+    mapping['bd'] = list(s4 - set(mapping['cf']))
+    mapping['eg'] = list(s8 - (set(mapping['a']) | set(mapping['cf']) | set(mapping['bd'])))
+    mapping['c']  = list( ((s069[0] | s069[1] | s069[2]) - (s069[0] & s069[1] & s069[2]) ) & set(mapping['cf']) )
+    mapping['f']  = list(set(mapping['cf']) - set(mapping['c']))
+    mapping['d']  = list( ((s069[0] | s069[1] | s069[2]) - (s069[0] & s069[1] & s069[2]) ) & set(mapping['bd']) )
+    mapping['b']  = list(set(mapping['bd']) - set(mapping['d']))
+    mapping['g']  = list( ( (s235[0] & s235[1] & s235[2]) 
+                           & ({'a', 'b', 'c', 'd', 'e', 'f', 'g'} 
+                              - (set(mapping['a']) | set(mapping['b']) | set(mapping['c']) | set(mapping['d']) | set(mapping['f']) ) )
+                           ))
+    mapping['e']  = list({'a', 'b', 'c', 'd', 'e', 'f', 'g'} 
+                         - (set(mapping['a']) | set(mapping['b']) | set(mapping['c']) | set(mapping['d']) | set(mapping['f']) | set(mapping['g']) ))
+    
+    # We need the mapping in the other direction, so invert it
+    invmaplist = [(mapping[k], k) for k in mapping.keys()]
+    invmap = {}
+    for l in invmaplist:
+        if len(l[0]) == 1:
+            invmap[l[0][0]] = [l[1]]
+    
+    # Use the (inverse) mapping to transfer the representation into standard representation
+    number = []
+    for o in out:
+        # print("o=", o)
+        digit = [] 
         for c in o:
-            c = ...
+            digit.append(invmap[c][0]) # digit is list of segment letters forming the current digit
+        # print("digit:", digit)len4
+        number.append(decode(digit))   # decode digit into a number
+
+    # print("Number decoded:", number)
+    totalsum += number[0]*1000+number[1]*100+number[2]*10+number[3]*1
+
+print(f"Task 2: Sum of all output numbers: {totalsum}")
